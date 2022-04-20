00cbaa4f94cbde5a8eaa007c40b89c69ce2f9372
scarl-ett <chdnjs7610@gmail.com>
2022-04-20 22:14:51 +0900

    useReducer, useMemo

--- src/Info.js
+++ src/Info.js
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value
+  };
+}
 
 const Info = () => {
   // const [name, setName] = useState('');
   // const [nickname, setNickname] = useState('');
-  const [visible, setVisible] = useState(false);
+  // const [visible, setVisible] = useState(false);
 
   // useEffect(() => {
   //   // console.log('랜더링이 완료되었습니다.');
@@ -29,31 +36,56 @@ const Info = () => {
   //   setNickname(e.target.value);
   // };
 
+  // return (
+  //   // <div>
+  //   //   <div>
+  //   //     <input value={name} onChange={onChangeName} />
+  //   //     <input value={nickname} onChange={onChangeNickname} />
+  //   //   </div>
+  //   //   <div>
+  //   //     <div>
+  //   //       <b>이름 :</b> {name}
+  //   //     </div>
+  //   //     <div>
+  //   //       <b>닉네임</b> {nickname}
+  //   //     </div>
+  //   //   </div>
+  //   // </div>
+  //   <div>
+  //     <button
+  //       onClick={() => {
+  //         setVisible(!visible);
+  //       }}
+  //     >
+  //       {visible ? '숨기기' : '보이기'}
+  //     </button>
+  //     <hr />
+  //     {visible && <Info />}
+  //   </div>
+  // );
+
+  const [state, dispatch] = useReducer(reducer, {
+    name: '',
+    nickname: ''
+  });
+
+  const { name, nickname } = state;
+  const onChange = e => {
+    dispatch(e.target);
+  };
+
   return (
-    // <div>
-    //   <div>
-    //     <input value={name} onChange={onChangeName} />
-    //     <input value={nickname} onChange={onChangeNickname} />
-    //   </div>
-    //   <div>
-    //     <div>
-    //       <b>이름 :</b> {name}
-    //     </div>
-    //     <div>
-    //       <b>닉네임</b> {nickname}
-    //     </div>
-    //   </div>
-    // </div>
     <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? '숨기기' : '보이기'}
-      </button>
-      <hr />
-      {visible && <Info />}
+      <input name="name" value={name} onChange={onChange} />
+      <input name="nickname" vaule={nickname} onChange={onChange} />
+      <div>
+        <div>
+          <b>이름 : </b>{name}
+        </div>
+        <div>
+          <b>닉네임 : </b>{nickname}
+        </div>
+      </div>
     </div>
   );
 };
